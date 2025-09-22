8dcc89ba01b97d8f89b4decc91c9f0d4a3708b6d
Ayushmanjha <ayushmanjha0510@gmail.com>
2025-09-22 21:16:35 +0530

    api

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -2,7 +2,7 @@ import { TokenManager } from '../utils/tokenManager';
 import { JWTUtils } from './jwtUtils';
 
 class HttpClient {
-  private baseURL = '/api';
+  private baseURL = "http://localhost:8080";
 
   private getHeaders(): HeadersInit {
     const token = TokenManager.getToken();
